dba94a629946f8fb279dc89f147c66818c260d17
Harvey Multani <harvey.multani@gmail.com>
2017-09-03 00:46:46 -0700

    Implemented Get/SendMessages and refactored

--- src/App.js
+++ src/App.js
@@ -11,12 +11,25 @@ class App extends Component {
       currentItem: '',
       username: '',
       items: []
-    }
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getMessages = this.getMessages.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
   }
 
-  componentDidMount() {
+  getMessages() {
+    // axios.get('http://localhost:5000/testproject-6177f/us-central1/returnMessages')
+    // .then(response => {
+    //   console.log(response);
+    //   this.setState({
+    //     items: response.data
+    //   });
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+
     const itemsRef = firebase.database().ref('items');
     itemsRef.on('value', (snapshot) => {
       let items = snapshot.val();
@@ -24,8 +37,8 @@ class App extends Component {
       for (let item in items) {
         newState.push({
           id: item,
-          title: items[item].title,
-          user: items[item].user
+          currentItem: items[item].currentItem,
+          username: items[item].username
         });
       }
       this.setState({
@@ -34,55 +47,67 @@ class App extends Component {
     });
   }
 
+  componentDidMount() {
+    this.getMessages();
+  }
+
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
     });
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
-    let item = {
-      title: this.state.currentItem,
-      user: this.state.username
-    }
-    var headers = {
-      'Content-Type': 'application/json'
-    }
-    axios.post('https://us-central1-testproject-6177f.cloudfunctions.net/addMessage', qs.stringify(item))
-    .then(function (response) {
+  sendMessage(data) {
+    let config = {
+      url: 'http://localhost:5000/testproject-6177f/us-central1/addMessage',
+      payload: qs.stringify(data)
+    };
+
+    axios.post(config.url, config.payload)
+    .then((response) => {
       console.log(response);
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
     });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let data = {
+      currentItem: this.state.currentItem,
+      username: this.state.username
+    };
+
+    this.sendMessage(data);
 
     this.setState({
       currentItem: '',
       username: ''
     });
-}
+  }
 
   render() {
     return (
-      <div className="App">
-        <section className="add-item">
+      <div className='App'>
+        <section className='add-item'>
           <form onSubmit={this.handleSubmit}>
-            <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-            <input type="text" name="currentItem" placeholder="What are you bringing ?" onChange={this.handleChange} value={this.state.currentItem} />
+            <input type='text' name='username' placeholder='Whats your name?' onChange={this.handleChange} value={this.state.username} />
+            <input type='text' name='currentItem' placeholder='What are you bringing?' onChange={this.handleChange} value={this.state.currentItem} />
             <button>Add Item</button>
           </form>
         </section>
         <section className='display-item'>
-          <div className="wrapper">
+          <div className='wrapper'>
             <ul>
               {this.state.items.map((item) => {
                 return (
                   <li key={item.id}>
-                    <h3>{item.title}</h3>
-                    <p>brought by: {item.user}</p>
+                    <h3>{item.currentItem}</h3>
+                    <p>brought by: {item.username}</p>
                   </li>
-                )
+                );
               })}
             </ul>
           </div>

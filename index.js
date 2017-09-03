abb0f2d101d365384db3ad702ac8183317dd171d
Harvey Multani <harvey.multani@gmail.com>
2017-09-03 00:43:36 -0700

    Implement addMessage

--- index.js
+++ index.js
@@ -3,24 +3,16 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const cors = require('cors')({
-	origin: true
-});
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-exports.addMessage = functions.https.onRequest((req, res) => {
-	// res.header("Access-Control-Allow-Origin", "*");
-  // res.header('Access-Control-Allow-Methods', 'GET, POST');
-	// res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-	cors((req, res, () => {
-		const original = req.body;
-		const title = req.body.title;
-		const user = req.body.user;
-		admin.database().ref('items').push({'title': title, 'user': user}).then(snapshot => {
-			res.redirect(303, snapshot.ref);
-		});
-  }));
+exports.addMessage = functions.https.onRequest((request, response) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  const username = request.body.username;
+  const currentItem = request.body.currentItem;
+  admin.database()
+  .ref('/items')
+  .push({username: username, currentItem: currentItem})
+  .then(() => {
+    response.sendStatus(201);
+  });
 });
 
 exports.returnMessages = functions.https.onRequest((request, response) => {

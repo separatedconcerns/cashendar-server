f5618f3c525771512ca1667736f45f6ada68c5e8
Landon Roberts <landonmarkroberts@gmail.com>
2017-09-04 18:21:52 -0500

    Implement .env vars

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 // const axios = require('axios');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+dotenv.config();
+
 admin.initializeApp(functions.config().firebase);
 
 exports.addMessage = functions.https.onRequest((request, response) => {

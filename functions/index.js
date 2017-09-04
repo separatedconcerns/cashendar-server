9dc9ab9b61d973e6f8989c24b4d85c7a7c39173b
Landon Roberts <landonmarkroberts@gmail.com>
2017-09-04 18:29:10 -0500

    Implement exchangePublicToken - saves access token to db

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 // const axios = require('axios');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const plaid = require('plaid');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -16,6 +17,35 @@ exports.addMessage = functions.https.onRequest((request, response) => {
   .then(() => {
     response.end();
   });
+});
+
+exports.exchangePublicToken = functions.https.onRequest((request, response) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  const publicToken = request.body.publicToken;
+  const plaidClient = new plaid.Client(
+    process.env.REACT_APP_PLAID_CLIENT_ID,
+    process.env.REACT_APP_PLAID_SECRET,
+    process.env.REACT_APP_PLAID_PUBLIC_KEY,
+    plaid.environments.sandbox);
+
+  plaidClient.exchangePublicToken(publicToken)
+    .then((successResponse) => {
+
+      let item_id = successResponse.item_id;
+      let access_token = successResponse.access_token;
+      let request_id = successResponse.request_id;
+
+      admin.database()
+      .ref('/users')
+      .push({item_id: item_id,
+        access_token: access_token,
+          request_id: request_id})
+      .then(() => {
+        response.end();
+      });
+    }).catch((error) => {
+      console.log(error);
+    });
 });
 
 exports.returnMessages = functions.https.onRequest((request, response) => {

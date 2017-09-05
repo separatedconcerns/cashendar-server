4148dec2eca8230986bc3a1b04e2799666d245dc
Landon Roberts <landonmarkroberts@gmail.com>
2017-09-04 22:41:14 -0500

    Implemented transaction persistence proof of concept

--- functions/index.js
+++ functions/index.js
@@ -3,21 +3,22 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const plaid = require('plaid');
 const dotenv = require('dotenv');
+const moment = require('moment');
 dotenv.config();
 
 admin.initializeApp(functions.config().firebase);
 
-exports.addMessage = functions.https.onRequest((request, response) => {
-  response.header('Access-Control-Allow-Origin', '*');
-  const username = request.body.username;
-  const currentItem = request.body.currentItem;
-  admin.database()
-  .ref('/items')
-  .push({username: username, currentItem: currentItem})
-  .then(() => {
-    response.end();
-  });
-});
+// exports.addMessage = functions.https.onRequest((request, response) => {
+//   response.header('Access-Control-Allow-Origin', '*');
+//   const username = request.body.username;
+//   const currentItem = request.body.currentItem;
+//   admin.database()
+//   .ref('/items')
+//   .push({username: username, currentItem: currentItem})
+//   .then(() => {
+//     response.end();
+//   });
+// });
 
 exports.exchangePublicToken = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
@@ -35,32 +36,89 @@ exports.exchangePublicToken = functions.https.onRequest((request, response) => {
       let access_token = successResponse.access_token;
       let request_id = successResponse.request_id;
 
+      let payload = {item_id: item_id,
+        access_token: access_token,
+        request_id: request_id,};
+
       admin.database()
       .ref('/users')
-      .push({item_id: item_id,
-        access_token: access_token,
-          request_id: request_id})
+      .push(payload)
       .then(() => {
         response.end();
       });
+
     }).catch((error) => {
       console.log(error);
     });
 });
 
-exports.returnMessages = functions.https.onRequest((request, response) => {
+exports.writeTransactions = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
-  const itemsRef = admin.database().ref('items');
-  itemsRef.on('value', (snapshot) => {
-    let items = snapshot.val();
-    let newState = [];
-    for (let item in items) {
-      newState.push({
-        id: item,
-        currentItem: items[item].currentItem,
-        username: items[item].username
+  const access_token = 'access-sandbox-2c10cb64-9ead-4128-9dfa-fe851df9ac5a';
+  const plaidClient = new plaid.Client(
+    process.env.REACT_APP_PLAID_CLIENT_ID,
+    process.env.REACT_APP_PLAID_SECRET,
+    process.env.REACT_APP_PLAID_PUBLIC_KEY,
+    plaid.environments.sandbox);
+
+  const now = moment();
+  const today = now.format('YYYY-MM-DD');
+  const thirtyDaysAgo = now.subtract(30, 'days').format('YYYY-MM-DD');
+
+  plaidClient.getTransactions(access_token, thirtyDaysAgo, today)
+    .then((successResponse) => {
+      // console.log('line 67: accounts', successResponse.accounts);
+      // console.log('line 68: transactions', successResponse.transactions);
+      // console.log('line 69: item', successResponse.item);
+      // console.log('line 70: request id', successResponse.request_id);
+
+      let item_id = successResponse.item.item_id;
+      let accounts = successResponse.accounts;
+      let request_id = successResponse.request_id;
+      let transactions = successResponse.transactions;
+
+      const ref = admin.database().ref('users');
+
+      ref.once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var childKey = childSnapshot.key;
+          var childData = childSnapshot.val();
+          console.log(childKey);
+          console.log(childData.item_id);
+          console.log(item_id);
+          console.log(childData.item_id === item_id);
+
+          admin.database()
+          .ref(`users/-KtEAsQdDs4LehAJ2Q5p/${item_id}`)
+          .push({transactions: transactions})
+          .then(() => {
+            response.end();
+          });
+
+        });
       });
-    }
-    response.end();
-  });
+
+      response.end();
+
+    }).catch((error) => {
+      console.log(error);
+    });
 });
+
+// exports.returnMessages = functions.https.onRequest((request, response) => {
+//   response.header('Access-Control-Allow-Origin', '*');
+//   const itemsRef = admin.database().ref('items');
+//   itemsRef.on('value', (snapshot) => {
+//     let items = snapshot.val();
+//     let newState = [];
+//     for (let item in items) {
+//       newState.push({
+//         id: item,
+//         currentItem: items[item].currentItem,
+//         username: items[item].username,
+//       });
+//     }
+//
+//     response.end();
+//   });
+// });

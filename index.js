7f716c4501da3d7fc18f719c6cb4017fc9354cf3
shaikathaque <shaikathaque4@gmail.com>
2017-09-05 12:07:59 -0700

    remove unnecessary comments and console logs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const axios = require('axios');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const plaid = require('plaid');
@@ -7,18 +6,6 @@ const moment = require('moment');
 dotenv.config();
 
 admin.initializeApp(functions.config().firebase);
-
-// exports.addMessage = functions.https.onRequest((request, response) => {
-//   response.header('Access-Control-Allow-Origin', '*');
-//   const username = request.body.username;
-//   const currentItem = request.body.currentItem;
-//   admin.database()
-//   .ref('/items')
-//   .push({username: username, currentItem: currentItem})
-//   .then(() => {
-//     response.end();
-//   });
-// });
 
 exports.exchangePublicToken = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
@@ -105,20 +92,3 @@ exports.writeTransactions = functions.https.onRequest((request, response) => {
     });
 });
 
-// exports.returnMessages = functions.https.onRequest((request, response) => {
-//   response.header('Access-Control-Allow-Origin', '*');
-//   const itemsRef = admin.database().ref('items');
-//   itemsRef.on('value', (snapshot) => {
-//     let items = snapshot.val();
-//     let newState = [];
-//     for (let item in items) {
-//       newState.push({
-//         id: item,
-//         currentItem: items[item].currentItem,
-//         username: items[item].username,
-//       });
-//     }
-//
-//     response.end();
-//   });
-// });

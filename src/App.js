af4d30f70a762f40a40c7cd2cc6d5a2925c96fd4
shaikathaque <shaikathaque4@gmail.com>
2017-09-05 12:07:59 -0700

    remove unnecessary comments and console logs

--- src/App.js
+++ src/App.js
@@ -8,18 +8,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentItem: '',
-      username: '',
-      items: [],
       user: null
     };
 
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
-    // this.handleChange = this.handleChange.bind(this);
     this.handleOnSuccess = this.handleOnSuccess.bind(this);
-    // this.getMessages = this.getMessages.bind(this);
-    // this.sendMessage = this.sendMessage.bind(this);
     this.exchangePublicToken = this.exchangePublicToken.bind(this);
   }
 
@@ -41,35 +35,6 @@ class App extends Component {
         });
       });
   }
-
-  // getMessages() {
-  //   // axios.get('http://localhost:5000/testproject-6177f/us-central1/returnMessages')
-  //   // .then(response => {
-  //   //   console.log(response);
-  //   //   this.setState({
-  //   //     items: response.data
-  //   //   });
-  //   // })
-  //   // .catch((error) => {
-  //   //   console.log(error);
-  //   // });
-  
-  //   const itemsRef = firebase.database().ref('items');
-  //   itemsRef.on('value', (snapshot) => {
-  //     let items = snapshot.val();
-  //     let newState = [];
-  //     for (let item in items) {
-  //       newState.push({
-  //         id: item,
-  //         currentItem: items[item].currentItem,
-  //         username: items[item].username
-  //       });
-  //     }
-  //     this.setState({
-  //       items: newState
-  //     });
-  //   });
-  // }
   
   componentDidMount() {
     auth.onAuthStateChanged((user) => {
@@ -77,17 +42,9 @@ class App extends Component {
         this.setState({ user });
       } 
     });
-    // this.getMessages();
   }
   
-  // handleChange(e) {
-  //   this.setState({
-  //     [e.target.name]: e.target.value
-  //   });
-  // }
-  //
   exchangePublicToken(publicToken) {
-    console.log('line 61');
     let config = {
       url: 'http://localhost:5000/testproject-6177f/us-central1/exchangePublicToken',
       payload: qs.stringify({publicToken: publicToken})
@@ -99,26 +56,8 @@ class App extends Component {
     });
   }
 
-  // handleSubmit(e) {
-  //   e.preventDefault();
-  
-  //   let data = {
-  //     currentItem: this.state.currentItem,
-  //     username: this.state.username
-  //   };
-  
-  //   this.sendMessage(data);
-  
-  //   this.setState({
-  //     currentItem: '',
-  //     username: ''
-  //   });
-  // }
-
   handleOnSuccess(token, metadata) {
     // send token to client server
-    console.log('token', token);
-    console.log('metadata', metadata);
     this.exchangePublicToken(token);
   }
 
@@ -141,29 +80,6 @@ class App extends Component {
           clientName='Wheres My Money'
           onSuccess={this.handleOnSuccess}
         />
-
-
-        {/* <section className='add-item'>
-          <form onSubmit={this.handleSubmit}>
-            <input type='text' name='username' placeholder='Whats your name?' onChange={this.handleChange} value={this.state.username} />
-            <input type='text' name='currentItem' placeholder='What are you bringing?' onChange={this.handleChange} value={this.state.currentItem} />
-            <button>Add Item</button>
-          </form>
-          </section>
-          <section className='display-item'>
-          <div className='wrapper'>
-            <ul>
-          {this.state.items.map((item) => {
-          return (
-          <li key={item.id}>
-          <h3>{item.currentItem}</h3>
-          <p>brought by: {item.username}</p>
-          </li>
-          );
-          })}
-            </ul>
-          </div>
-        </section> */}
       </div>
     );
   }

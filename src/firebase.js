6194a86c0e2f90b06043b8d49fb0d610802c93f4
shaikathaque <shaikathaque4@gmail.com>
2017-09-04 18:30:57 -0700

    add google auth provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,17 @@
 import firebase from 'firebase';
+import { authDomain, apiKey } from './config.js';
 require('dotenv').config();
+
 // Initialize Firebase
 var config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  apiKey: apiKey,
+  authDomain: authDomain,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
 export default firebase;

534cbb35c242c2675b0f18fde4c40f95e0354d23
Harvey Multani <harvey.multani@gmail.com>
2017-09-08 14:05:01 -0700

    Hack for private key

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const axios = require('axios');
 const plaid = require('plaid');
 // const plaidClient = require('./plaidClient.js')
 
+let privateKey = process.env.REACT_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+
 admin.initializeApp({
   credential: admin.credential.cert({
  "type": process.env.REACT_APP_FIREBASE_TYPE,
  "project_id": process.env.REACT_APP_FIREBASE_PROJECT_ID,
  "private_key_id": process.env.REACT_APP_FIREBASE_PRIVATE_KEY_ID,
- "private_key": process.env.REACT_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+ "private_key": privateKey,
  "client_email": process.env.REACT_APP_FIREBASE_CLIENT_EMAIL,
  "client_id": process.env.REACT_APP_FIREBASE_CLIENT_ID,
  "auth_uri": process.env.REACT_APP_FIREBASE_AUTH_URI,

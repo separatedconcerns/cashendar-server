8eef5e07dcd3d261c0015f2d84c2f24075b8bc72
Harvey Multani <harvey.multani@gmail.com>
2017-09-08 13:02:07 -0700

    Remove new lines from private key

--- functions/index.js
+++ functions/index.js
@@ -5,23 +5,20 @@ const admin = require('firebase-admin');
 const moment = require('moment');
 const axios = require('axios');
 const plaidClient = require('./plaidClient.js')
-const serviceAccount = require('./serviceAccountKey.json');
-
 
 admin.initializeApp({
   credential: admin.credential.cert({
  "type": process.env.REACT_APP_FIREBASE_TYPE,
  "project_id": process.env.REACT_APP_FIREBASE_PROJECT_ID,
  "private_key_id": process.env.REACT_APP_FIREBASE_PRIVATE_KEY_ID,
- "private_key": process.env.REACT_APP_FIREBASE_PRIVATE_KEY,
+ "private_key": process.env.REACT_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
  "client_email": process.env.REACT_APP_FIREBASE_CLIENT_EMAIL,
  "client_id": process.env.REACT_APP_FIREBASE_CLIENT_ID,
  "auth_uri": process.env.REACT_APP_FIREBASE_AUTH_URI,
  "token_uri": process.env.REACT_APP_FIREBASE_TOKEN_URI,
  "auth_provider_x509_cert_url": process.env.REACT_APP_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
  "client_x509_cert_url": process.env.REACT_APP_FIREBASE_CLIENT_X509_CERT_URL
-}
-),
+}),
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 });
 

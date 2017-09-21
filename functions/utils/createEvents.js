00fde12a032128d2f0d7941cfa3623cc3a66fa4b
Harvey Multani <harvey.multani@gmail.com>
2017-09-20 20:20:39 -0700

    Undo breaking other endpoint

--- functions/utils/createEvents.js
+++ functions/utils/createEvents.js
@@ -4,17 +4,17 @@ const google = require('googleapis');
 const Promise = require('bluebird');
 
 let createEvents = (auth, calendarId, uniqueUserId) => {
-    const config = {
-      url: 'http://localhost:5000/testproject-6177f/us-central1/getDailySpendingAndTransactions',
-      payload: { uniqueUserId },
-    };
-    axios.post(config.url, config.payload)
-    .then(transactionsByDate => {
-      const dailySpending = transactionsByDate.data;
-      for (let date in dailySpending) {
-        const sum = Math.round(dailySpending[date].sum);
-        const list = dailySpending[date].list.join('\n');
-        const event = {
+  const config = {
+    url: 'http://localhost:5000/testproject-6177f/us-central1/getDailySpendingAndTransactions',
+    payload: { uniqueUserId },
+  };
+  axios.post(config.url, config.payload)
+  .then(transactionsByDate => {
+    const dailySpending = transactionsByDate.data;
+    for (const date in dailySpending) {
+      const sum = Math.round(dailySpending[date].sum);
+      const list = dailySpending[date].list.join('\n');
+      const event = {
           summary: `Spent $${sum}`,
           location: 'See description for transaction details!',
           description: `Transactions: \n\  ${list}`,
@@ -27,17 +27,18 @@ let createEvents = (auth, calendarId, uniqueUserId) => {
             timeZone: 'America/Los_Angeles',
           },
         };
-        const targetCal = {
-          auth,
-          calendarId,
-          resource: event,
-        };
-        const eventInsert = Promise.promisify(google.calendar('v3').events.insert);
-        eventInsert(targetCal)
-        .catch(e => response.end(`there was an error contacting Google Calendar${e}`));
-      }
-    }).then(console.log('39, done'))
-    .catch(e => console.log('40', e));
-  }
+      const targetCal = {
+        auth,
+        calendarId,
+        resource: event
+      };
+      const eventInsert = Promise.promisify(google.calendar('v3').events.insert);
+      eventInsert(targetCal)
+      // .then(result => console.log('37', result))
+      .catch(e => console.log(`there was an error contacting Google Calendar${e}`));
+    }
+  }).then(console.log('40, done'))
+  .catch(e => console.log('41', e))
+}
 
-  module.exports = createEvents;
+module.exports = createEvents;

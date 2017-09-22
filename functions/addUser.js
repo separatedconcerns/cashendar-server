764e7684529d995e16f21751f28dc4e8acf8806a
Harvey Multani <harvey.multani@gmail.com>
2017-09-21 17:20:08 -0700

    Flatten promise chain

--- functions/addUser.js
+++ functions/addUser.js
@@ -2,52 +2,60 @@ const functions = require('firebase-functions');
 const admin = require('./apiClients/firebaseClient.js');
 const axios = require('axios');
 
-// eslint-disable-next-line
 const addUser = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
   const idToken = request.body.idToken;
   const OAuthToken = request.body.OAuthToken;
+  let uniqueUserId;
 
-  // verifies firebase idToken
-  admin.auth().verifyIdToken(idToken)
+  admin.auth()
+    .verifyIdToken(idToken)
     .then((decodedToken) => {
-      const uniqueUserId = decodedToken.uid;
-      const ref = admin.database().ref(`users/${uniqueUserId}`);
-
-      // searches for uniqueUserId in db
-      // if user exists response is ended
-      // otherwise a new user is created in db,
-      // a new calendar is created,
-      // calendarId is saved in db
-      ref.once('value')
-        .then((snapshot) => {
-          if (snapshot.exists()) { response.send({ userExists: true }); } else {
-            admin.auth().getUser(uniqueUserId)
-              .then((userRecord) => {
-                const user = userRecord.toJSON();
-                const payload = {
-                  email: user.email,
-                  name: user.displayName,
-                  OAuthToken,
-                };
-                admin.database().ref(`users/${uniqueUserId}`).set(payload)
-                  .then(() => {
-                    const config = {
-                      url: 'http://localhost:5000/testproject-6177f/us-central1/createNewCalendar',
-                      payload: { OAuthToken },
-                    };
-                    axios.post(config.url, config.payload)
-                      .then((calendar) => {
-                        const calId = calendar.data.id;
-                        const calName = calendar.data.summary;
-                        admin.database().ref(`users/${uniqueUserId}`).update({ calendarId: calId, calendarName: calName });
-                      });
-                  });
-              }).then(response.end(''))
-              .catch(error => console.log('Error fetching user data:', error));
-          }
-        });
-    });
+      uniqueUserId = decodedToken.uid;
+    })
+    .then(() => admin.database()
+      .ref(`users/${uniqueUserId}`)
+      .once('value')
+      .then(snapshot => snapshot.exists()))
+    .then((exists) => {
+      if (exists) {
+        response.send({ userExists: true });
+      } else {
+        return admin.auth().getUser(uniqueUserId);
+      }
+    })
+    .then((userRecord) => {
+      const user = userRecord.toJSON();
+      return {
+        email: user.email,
+        name: user.displayName,
+        OAuthToken,
+      };
+    })
+    .then((payload) => {
+      admin.database()
+        .ref(`users/${uniqueUserId}`)
+        .set(payload);
+    })
+    .then(() => {
+      const config = {
+        url: 'http://localhost:5000/testproject-6177f/us-central1/createNewCalendar',
+        payload: { OAuthToken, uniqueUserId },
+      };
+      return config;
+    })
+    .then(config => axios.post(config.url, config.payload)
+      .then(calendar => ({
+        calId: calendar.data.id,
+        calName: calendar.data.summary,
+      })))
+    .then((configCal) => {
+      admin.database()
+        .ref(`users/${uniqueUserId}`)
+        .update({ calendarId: configCal.calId, calendarName: configCal.calName });
+    })
+    .then(response.end())
+    .catch(error => console.log('Error fetching user data:', error));
 });
 
 module.exports = addUser;

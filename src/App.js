9fd99bce59137ab638f72085980f847c2c04d1b5
Harvey Multani <harvey.multani@gmail.com>
2017-09-22 21:51:46 -0700

    Move hosts to env and fix linting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { auth, provider } from './firebase.js';
 import axios from 'axios';
 import qs from 'qs';
-var PlaidLink = require('react-plaid-link');
+import { auth, provider } from './firebase';
+
+const PlaidLink = require('react-plaid-link');
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       user: null,
-      OAuthToken: null
+      OAuthToken: null,
     };
 
     this.login = this.login.bind(this);
@@ -22,52 +23,50 @@ class App extends Component {
   componentDidMount() {
     auth.onAuthStateChanged((user) => {
       if (user) {
-        this.setState({user});
+        this.setState({ user });
       }
     });
   }
 
   login() {
     auth.signInWithPopup(provider)
-      .then(result => {
+      .then((result) => {
         const user = result.user;
         const OAuthToken = result.credential.accessToken;
-        this.setState({user, OAuthToken});
+        this.setState({ user, OAuthToken });
         this.verifyUser(OAuthToken);
-      })
+      });
   }
 
   verifyUser(OAuthToken) {
     auth.currentUser.getIdToken()
-      .then(idToken => {
-          const config = {
-            url: 'http://localhost:5000/testproject-6177f/us-central1/addUser',
-            payload: qs.stringify({idToken: idToken, OAuthToken: OAuthToken})
-          };
-          axios.post(config.url, config.payload)
-            .catch(err => console.log(err));
-        });
+      .then((idToken) => {
+        const config = {
+          url: `${process.env.REACT_APP_HOST}addUser`,
+          payload: qs.stringify({ idToken, OAuthToken }),
+        };
+        axios.post(config.url, config.payload)
+          .catch(err => console.log(err));
+      });
   }
 
   handleOnSuccess(token, metadata) {
     // send token to client server
-    let institution = metadata.institution
+    const institution = metadata.institution;
     this.exchangePublicToken(token, institution);
   }
 
   exchangePublicToken(publicToken, institution) {
-
-    let config = {
-      url: 'http://localhost:5000/testproject-6177f/us-central1/exchangePublicToken',
+    const config = {
+      url: `${process.env.REACT_APP_HOST}exchangePublicToken`,
       payload: qs.stringify({
-        publicToken: publicToken,
+        publicToken,
         uniqueUserId: auth.currentUser.uid,
-        institution: institution
-      })
+        institution,
+      }),
     };
     axios.post(config.url, config.payload)
-    .then(() => this.getTransactionsFromDatabase())
-    .catch(error => { console.log(error);});
+      .catch((error) => { console.log(error); });
   }
 
   logout() {
@@ -76,31 +75,31 @@ class App extends Component {
         this.setState({
           user: null,
           transactions: [],
-          transactionSums: {}
+          transactionSums: {},
         });
       });
   }
 
   deleteProfile() {
-    let config = {
-      url: 'http://localhost:5000/testproject-6177f/us-central1/deleteUserProfile',
-      payload: qs.stringify({uniqueUserId: this.state.user.uid})
-    }
+    const config = {
+      url: `${process.env.REACT_APP_HOST}deleteUserProfile`,
+      payload: qs.stringify({ uniqueUserId: this.state.user.uid }),
+    };
     axios.post(config.url, config.payload)
-    .then(response => console.log(response))
-    .then(() => {this.logout()})
-    .catch(e => console.log(e));
+      .then(response => console.log(response))
+      .then(() => { this.logout(); })
+      .catch(e => console.log(e));
   }
 
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Where's My Money</h1>
-        <div className='wrapper'>
+        <div className="wrapper">
           {this.state.user ?
             <button onClick={this.logout}>Log Out</button>
-          :
-          <button onClick={this.login}>Log In</button>
+            :
+            <button onClick={this.login}>Log In</button>
           }
         </div>
 
@@ -109,19 +108,19 @@ class App extends Component {
             <div>{this.state.user.email}</div>
             <PlaidLink
               publicKey={process.env.REACT_APP_PLAID_PUBLIC_KEY}
-              product='connect'
+              product="connect"
               webhook={process.env.REACT_APP_WEBHOOK}
-              env='sandbox'
-              clientName='Wheres My Money'
+              env="sandbox"
+              clientName="Wheres My Money"
               onSuccess={this.handleOnSuccess}
             />
           </div>
-        :
-        <div>Log in to link account</div>
+          :
+          <div>Log in to link account</div>
         }
         {this.state.user ?
           <button onClick={this.deleteProfile}>Delete Profile</button> :
-          <div></div>
+          <div />
         }
       </div>
     );

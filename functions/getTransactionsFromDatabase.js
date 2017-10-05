056269166c7dd808a419bc17698549c9c964de1b
Landon Roberts <landonmarkroberts@gmail.com>
2017-10-05 16:36:36 -0500

    Refactor to handle getting transactions stored by I.D. from db

--- functions/getTransactionsFromDatabase.js
+++ functions/getTransactionsFromDatabase.js
@@ -4,14 +4,14 @@ const admin = require('./apiClients/firebaseClient.js');
 const getTransactionsFromDatabase = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
   const uniqueUserId = request.body.uniqueUserId;
-  let allTransactions = [];
+  const allTransactions = {};
   admin.database()
     .ref('items/')
     .once('value')
     .then((snapshot) => {
       snapshot.forEach((childSnapshot) => {
         if (childSnapshot.val().uniqueUserId === uniqueUserId) {
-          allTransactions = allTransactions.concat(childSnapshot.val().transactions);
+          Object.assign(allTransactions, childSnapshot.val().transactions);
         }
       });
     })

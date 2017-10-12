14456c35c99d73cd2db0685c1866113de593526e
Landon Roberts <landonmarkroberts@gmail.com>
2017-10-12 18:13:29 -0500

    Refactor packageTransactionsByDate to deliver transactions by date with nested transaction id objects

--- functions/utils/packageTransactionsByDate.js
+++ functions/utils/packageTransactionsByDate.js
@@ -3,8 +3,8 @@ const packageTransactionsByDate = (transactions) => {
   const transactionsByDate = {};
 
   transactions.forEach((transaction) => {
-    transactionsByDate[transaction.date] = transactionsByDate[transaction.date] || [];
-    transactionsByDate[transaction.date].push(transaction);
+    transactionsByDate[transaction.date] = transactionsByDate[transaction.date] || {};
+    transactionsByDate[transaction.date][transaction.transaction_id] = transaction;
   });
 
   return transactionsByDate;

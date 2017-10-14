63cb8fcb79a57d1c77388bfe1d425094595960c3
Landon Roberts <landonmarkroberts@gmail.com>
2017-10-13 22:36:39 -0500

    Refactor webhook to end if item id doesnt exist in db

--- functions/plaidWebHook.js
+++ functions/plaidWebHook.js
@@ -14,19 +14,24 @@ const plaidWebHook = functions.https.onRequest((request, response) => {
   } else {
     const ref = admin.database().ref(`items/${itemId}`);
     ref.once('value')
-      .then(snapshot => ({
-        url: `${process.env.HOST}getTransactionsFromPlaid`,
-        payload: {
-          access_token: snapshot.val().access_token,
-          uniqueUserId: snapshot.val().uniqueUserId,
-          newTransactions,
-        },
-      })).then((config) => {
-        axios.post(config.url, config.payload)
-          .then(() => {
-            axios.post(`${process.env.HOST}addCalendarEvents`, config.payload)
-              .then(response.end());
-          });
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const config = {
+            url: `${process.env.HOST}getTransactionsFromPlaid`,
+            payload: {
+              access_token: snapshot.val().access_token,
+              uniqueUserId: snapshot.val().uniqueUserId,
+              newTransactions,
+            },
+          };
+          axios.post(config.url, config.payload)
+            .then(() => {
+              axios.post(`${process.env.HOST}addCalendarEvents`, config.payload)
+                .then(response.end());
+            });
+        } else {
+          response.end();
+        }
       });
   }
 });

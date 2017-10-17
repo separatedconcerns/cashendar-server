147785265c841279b32eaf8e8b8f46623a855726
shaikathaque <shaikathaque4@gmail.com>
2017-10-17 09:46:32 -0700

    return userid if user exists

--- functions/addUser.js
+++ functions/addUser.js
@@ -12,45 +12,42 @@ const addUser = functions.https.onRequest((request, response) => {
   verifyIdToken(idToken)
     .then((result) => {
       uniqueUserId = result;
-      user.doesUserExist(uniqueUserId);
-    })
-    .then((exists) => {
-      if (exists) {
-        response.send({ userExists: true });
+      if (user.doesUserExist(uniqueUserId)) {
+        response.send({ uniqueUserId });
       } else {
-        return user.getUserProfile(uniqueUserId);
+        user.getUserProfile(uniqueUserId)
+          .then((userRecord) => {
+          // todo: move userRecord.toJSON to controller also? 
+            const userProfile = userRecord.toJSON();
+            return {
+              email: userProfile.email,
+              name: userProfile.displayName,
+              OAuthToken,
+            };
+          })
+          .then((payload) => {
+            user.initializeUser(uniqueUserId, payload);
+          })
+          .then(() => {
+            const config = {
+              url: `${process.env.HOST}createNewCalendar`,
+              payload: { OAuthToken, uniqueUserId },
+            };
+            return config;
+          })
+          .then(config => axios.post(config.url, config.payload)
+            .then(calendar => ({
+              calId: calendar.data.id,
+              calName: calendar.data.summary,
+            })))
+          .then((configCal) => {
+            const userCalendarDetails = { calendarId: configCal.calId, calendarName: configCal.calName };
+            user.updateUser(uniqueUserId, userCalendarDetails);
+          })
+          .then(response.end())
+          .catch(error => console.log('Error fetching user data:', error));
       }
-    })
-    .then((userRecord) => {
-      // todo: move userRecord.toJSON to controller also? 
-      const userProfile = userRecord.toJSON();
-      return {
-        email: userProfile.email,
-        name: userProfile.displayName,
-        OAuthToken,
-      };
-    })
-    .then((payload) => {
-      user.initializeUser(uniqueUserId, payload);
-    })
-    .then(() => {
-      const config = {
-        url: `${process.env.HOST}createNewCalendar`,
-        payload: { OAuthToken, uniqueUserId },
-      };
-      return config;
-    })
-    .then(config => axios.post(config.url, config.payload)
-      .then(calendar => ({
-        calId: calendar.data.id,
-        calName: calendar.data.summary,
-      })))
-    .then((configCal) => {
-      const userCalendarDetails = { calendarId: configCal.calId, calendarName: configCal.calName };
-      user.updateUser(uniqueUserId, userCalendarDetails);
-    })
-    .then(response.end())
-    .catch(error => console.log('Error fetching user data:', error));
+    });
 });
 
 module.exports = addUser;

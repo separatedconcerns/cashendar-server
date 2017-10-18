0a445a6c728373fbfdd8310cf72d3f4560c7e8cd
Landon Roberts <landonmarkroberts@gmail.com>
2017-10-18 03:43:18 -0500

    Wrote cloud function to delete duplicate calendar events

---
+++ functions/deleteDuplicateEventsInCalendar.js
@@ -0,0 +1,49 @@
+const functions = require('firebase-functions');
+const admin = require('./apiClients/firebaseClient.js');
+const axios = require('axios');
+const google = require('googleapis');
+const Promise = require('bluebird');
+const googleClient = require('./apiClients/googleClient.js');
+const packageEventsToDelete = require('./utils/packageEventsToDelete.js');
+
+const deleteDuplicateEventsInCalendar = functions.https.onRequest((request, response) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  const googleClientAuthorize = Promise.promisify(googleClient.authorize);
+  const calendarId = request.body.calendarId;
+  const OAuthToken = request.body.OAuthToken;
+  const eventsToDelete = request.body.eventsToDelete;
+
+  const deleteDuplicateEvents = (auth) => {
+    const deleteEvent = Promise.promisify(google.calendar('v3').events.delete);
+    const packageEventsToDeleteProm = Promise.method(packageEventsToDelete);
+
+    packageEventsToDeleteProm(auth, calendarId, eventsToDelete)
+      .then((packagedEvents) => {
+        let i = 0;
+        let eventsToBeDeleted = packagedEvents.length;
+        console.log(`${eventsToBeDeleted} events to be deleted`);
+
+        const deleteEvents = setInterval(() => {
+          if (i <= packagedEvents.length - 1) {
+            deleteEvent(packagedEvents[i])
+              .catch((e) => {
+                eventsToBeDeleted -= 1;
+                console.log('EVENT NOT DELETED!:', e);
+              });
+            i += 1;
+            console.log(i);
+          } else {
+            console.log(`${eventsToBeDeleted} of ${packagedEvents.length} events have been deleted`);
+            console.log('EVENTS UP-TO-DATE AND PROGRAM LOOP COMPLETE!'); 
+            clearInterval(deleteEvents);
+          }
+        }, 400);
+      });
+  };
+  
+  googleClientAuthorize(OAuthToken, deleteDuplicateEvents)
+    .then(response.end())
+    .catch(e => console.log('deleteDuplicateEvents FAILURE!:', e));
+});
+
+module.exports = deleteDuplicateEventsInCalendar;

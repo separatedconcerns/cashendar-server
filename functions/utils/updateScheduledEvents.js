fac40bd423de25613705d9c10d388b3dd59936db
Landon Roberts <landonmarkroberts@gmail.com>
2017-10-18 03:49:52 -0500

    Updates users/userId/sheduledEvents with newly sheduled events

--- functions/utils/updateScheduledEvents.js
+++ functions/utils/updateScheduledEvents.js
@@ -3,7 +3,8 @@ const admin = require('../apiClients/firebaseClient.js');
 const updateScheduledEvents = (uniqueUserId, newEvents) => {
   admin.database()
     .ref(`users/${uniqueUserId}/scheduledEvents`)
-    .update(newEvents);
+    .update(newEvents)
+    .then({ newEvents });
 };
 
 module.exports = updateScheduledEvents;

4ae12f7402f83b61c8daa7e9fa6aff8138c1606f
Harvey Multani <harvey.multani@gmail.com>
2017-10-31 12:29:59 -0700

    Remove REACT_APP prefixes from envs

--- functions/apiClients/firebaseClient.js
+++ functions/apiClients/firebaseClient.js
@@ -2,18 +2,18 @@ const admin = require('firebase-admin');
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.REACT_APP_FIREBASE_TYPE,
-    project_id: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    private_key_id: process.env.REACT_APP_FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.REACT_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.REACT_APP_FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.REACT_APP_FIREBASE_CLIENT_ID,
-    auth_uri: process.env.REACT_APP_FIREBASE_AUTH_URI,
-    token_uri: process.env.REACT_APP_FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.REACT_APP_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.REACT_APP_FIREBASE_CLIENT_X509_CERT_URL,
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
   }),
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 module.exports = admin;

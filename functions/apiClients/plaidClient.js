7e668e0cd243826fcae7d66765411cc7cc33b603
Harvey Multani <harvey.multani@gmail.com>
2017-10-31 12:30:15 -0700

    Remove REACT_APP prefixes and move environment into ENV

--- functions/apiClients/plaidClient.js
+++ functions/apiClients/plaidClient.js
@@ -1,9 +1,9 @@
 const plaid = require('plaid');
 
 const plaidClient = new plaid.Client(
-  process.env.REACT_APP_PLAID_CLIENT_ID,
-  process.env.REACT_APP_PLAID_SECRET,
-  process.env.REACT_APP_PLAID_PUBLIC_KEY,
-  plaid.environments.sandbox);
+  process.env.PLAID_CLIENT_ID,
+  process.env.PLAID_SECRET,
+  process.env.PLAID_PUBLIC_KEY,
+  process.env.PLAID_ENV);
 
 module.exports = plaidClient;

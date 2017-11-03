4807dd489cb34703614caa5563176aa00161d77f
Landon Roberts <landonmarkroberts@gmail.com>
2017-11-02 19:26:28 -0500

    Uncomment previous commit changes

--- functions/removeTransactionsFromDb.js
+++ functions/removeTransactionsFromDb.js
@@ -9,9 +9,9 @@ const removeTransactionsFromDb = functions.https.onRequest((request, response) =
   const ref = admin.database().ref(`items/${itemId}/transactions`);
   const uniqueUserId; 
   
-  // admin.database().ref(`items/${itemId}/uniqueUserId`)
-  //   .once('value')
-  //   .then(snapshot => uniqueUserId = snapshot.val());
+  admin.database().ref(`items/${itemId}/uniqueUserId`)
+    .once('value')
+    .then(snapshot => uniqueUserId = snapshot.val());
 
   ref.once('value')
     .then((snapshot) => {
@@ -41,11 +41,10 @@ const removeTransactionsFromDb = functions.https.onRequest((request, response) =
 
         if (counter <= 0) {
           console.log(transactionDatesToRemove.length, 'transactions have been removed from database.');
-          // admin.database().ref(`users/${uniqueUserId}/datesToSchedule`)
-          //   .once('value')
-          //   .set(transactionDates)
-          //   .then(response.end())
-          response.end();
+          admin.database().ref(`users/${uniqueUserId}/datesToSchedule`)
+            .once('value')
+            .set(transactionDates)
+            .then(response.end());
         }
       });
     });

bd7b62cd5b405a9c8da59e4ece3f63a58293c2f2
Harvey Multani <harvey.multani@gmail.com>
2017-11-10 22:41:47 -0800

    Move dates to schedule query to controller

--- functions/utils/deleteDuplicateEventsFlow.js
+++ functions/utils/deleteDuplicateEventsFlow.js
@@ -1,19 +1,16 @@
 const axios = require('axios');
-const admin = require('../apiClients/firebaseClient.js');
+const user = require('../controllers/userController.js');
 const Promise = require('bluebird');
 const updateScheduledEvents = require('./updateScheduledEvents.js');
 
 const deleteDuplicateEventsFlow = (uniqueUserId, newEvents) => {
-
-  admin.database().ref(`users/${uniqueUserId}/datesToSchedule`)
-    .once('value')
-    .then((snapshot) => {
-      const datesToSchedule = snapshot.val();
+  user.getDatesToScheduleFromDB(uniqueUserId)
+    .then((datesToSchedule) => {
       datesToSchedule.forEach((date) => {
         newEvents[date] = newEvents[date] || null;
       });
     });
-  
+
   setTimeout(() => {
     const config = {
       url: `${process.env.HOST}createEventsToDeleteArrayInDb`,
@@ -24,12 +21,10 @@ const deleteDuplicateEventsFlow = (uniqueUserId, newEvents) => {
     };
     axios.post(config.url, config.payload)
       .catch(e => console.log('Events to delete array not created!:', e))
-      .then((calId_eventsToDelete_OAuthToken) => {
-        return {
+      .then((calId_eventsToDelete_OAuthToken) => ({
           url: `${process.env.HOST}deleteDuplicateEventsInCalendar`,
           payload: calId_eventsToDelete_OAuthToken.data,
-        };
-      })
+        }))
       .then((config2) => {
         axios.post(config2.url, config2.payload)
           .then(() => {

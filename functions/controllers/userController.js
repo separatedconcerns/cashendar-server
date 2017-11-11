f503a6690d021eeac624ffb71a19274134ba227c
Harvey Multani <harvey.multani@gmail.com>
2017-11-10 17:39:35 -0800

    Build out user collection queries

--- functions/controllers/userController.js
+++ functions/controllers/userController.js
@@ -1,33 +1,77 @@
 const admin = require('../apiClients/firebaseClient');
 
+// const userRef = admin.database()
+//   .ref(`users/${uniqueUserId}`);
+
+const userRef = uniqueUserId => admin.database()
+  .ref(`users/${uniqueUserId}`);
+
 const doesUserExist = uniqueUserId =>
   new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`users/${uniqueUserId}`)
-      .once('value')
+    userRef(uniqueUserId).once('value')
       .then(snapshot => resolve(snapshot.exists()))
       .catch(err => reject(err));
   });
 
-// TODO: return promise
-const getUserProfile = uniqueUserId => admin.auth().getUser(uniqueUserId);
+const getUserFromDB = uniqueUserId =>
+  new Promise((resolve, reject) => {
+    userRef(uniqueUserId).once('value')
+      .then(snapshot => resolve(snapshot.val()))
+      .catch(err => reject(err));
+  });
 
-// TODO: return promise
-const initializeUser = (uniqueUserId, payload) => {
-  admin.database()
-    .ref(`users/${uniqueUserId}`)
-    .set(payload);
-};
+const getUserItems= uniqueUserId =>
+  new Promise((resolve, reject) => {
+    admin.database()
+      .ref(`users/${uniqueUserId}/items`).once('value')
+      .then(snapshot => resolve(snapshot))
+      .catch(err => reject(err));
+  });
 
-// TODO: return promise
-const updateUser = (uniqueUserId, newData) => {
-  admin.database()
-    .ref(`users/${uniqueUserId}`)
-    .update(newData);
-};
+const getUserProfile = uniqueUserId =>
+  new Promise((resolve, reject) => {
+    admin.auth()
+      .getUser(uniqueUserId)
+      .then(userRecord => resolve(userRecord.toJSON()))
+      .catch(err => reject(err));
+  });
+
+const initializeUser = (uniqueUserId, payload) =>
+  new Promise((resolve, reject) => {
+    userRef(uniqueUserId).set(payload)
+      .then(resolve())
+      .catch(err => reject(err));
+  });
+
+const updateUser = (uniqueUserId, newData) =>
+  new Promise((resolve, reject) => {
+    userRef(uniqueUserId).update(newData)
+      .then(resolve())
+      .catch(err => reject(err));
+  });
+
+const deleteUserInAuth = uniqueUserId =>
+  new Promise((resolve, reject) => {
+    admin.auth()
+      .deleteUser(uniqueUserId)
+      .then(resolve())
+      .catch(err => reject(err));
+  });
+
+const deleteUserFromDB = uniqueUserId =>
+  new Promise((resolve, reject) => {
+    userRef(uniqueUserId).remove()
+      .then(resolve())
+      .catch(err => reject(err));
+  });
+
 
 module.exports = { doesUserExist,
+  getUserFromDB,
+  getUserItems,
   getUserProfile,
   initializeUser,
   updateUser,
+  deleteUserInAuth,
+  deleteUserFromDB,
 };

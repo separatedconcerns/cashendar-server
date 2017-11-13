89059ee74bc80d25f6a792c25ed9b49a4a2a5cc5
Landon Roberts <landonmarkroberts@gmail.com>
2017-11-13 14:29:52 -0600

    Refactor getTransactionsFromPlaid to paginate transactions if there are more than 500 transactions

--- functions/getTransactionsFromPlaid.js
+++ functions/getTransactionsFromPlaid.js
@@ -8,7 +8,7 @@ const packageTransactionsByDate = Promise.method(require('./utils/packageTransac
 
 const getTransactionsFromPlaid = functions.https.onRequest((request, response) => {
   const accessToken = request.body.access_token;
-  const newTransactions = request.body.newTransactions;
+  const numOfNewTransactions = request.body.newTransactions;
   // const uniqueUserId = request.body.uniqueUserId;
   const now = moment();
   const today = now.format('YYYY-MM-DD');
@@ -18,8 +18,45 @@ const getTransactionsFromPlaid = functions.https.onRequest((request, response) =
     .then((successResponse) => {
       // let uniqueUserId;
       const itemId = successResponse.item.item_id;
+  const plaidGetTransactions = new Promise((resolve, reject) => {
+    const transactionsObj = {
+      transactions: [],
+      itemId: null,
+    };
+    let numLoops = Math.ceil(numOfNewTransactions / 500);
+    let count = numOfNewTransactions > 500 ? numOfNewTransactions % 500 : numOfNewTransactions;
+    let offset = 0;
+    const pingPlaid = setInterval(() => {
+      plaidClient.getTransactions(accessToken, daysAgo, today, { count, offset })
+        .then((plaidResponse) => {
+          transactionsObj.transactions = transactionsObj.transactions.concat(plaidResponse.transactions);
+          transactionsObj.itemId = transactionsObj.itemId || plaidResponse.item.item_id;
+          console.log(transactionsObj.transactions.length, '   ', numOfNewTransactions);
+        })
+        .catch((e) => {
+          console.log('pingPlaid ERROR!: ', e);
+          reject('Error inside pingPlaid');
+        });
+      offset += count;
+      count = 500;
+      numLoops -= 1;
+      if (numLoops < 1) {
+        console.log(transactionsObj.transactions.length >= numOfNewTransactions);
+        setTimeout(() => {
+          resolve(transactionsObj);
+        }, 2000);
+        clearInterval(pingPlaid);
+      }
+    }, 100);
+  });
+
+  plaidGetTransactions
+    .then((transactionsObj) => {
+      console.log('transactionsObj', transactionsObj.transactions.slice(0, 3));
+      let uniqueUserId;
+      const itemId = transactionsObj.itemId;
       // const institutionId = successResponse.item.institution_id;
-      const transactions = successResponse.transactions;
+      const transactions = transactionsObj.transactions;
       // const accounts = successResponse.accounts;
       // const requestId = successResponse.request_id;
       console.log('getTransactionsFromPlaid total transactions:', transactions.length);

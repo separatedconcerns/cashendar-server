98becd38eabd978e8cb0e29a27d5f287cc070f57
Landon Roberts <landonmarkroberts@gmail.com>
2017-11-13 14:28:59 -0600

    change plaid public token to development

--- functions/apiClients/plaidClient.js
+++ functions/apiClients/plaidClient.js
@@ -4,6 +4,6 @@ const plaidClient = new plaid.Client(
   process.env.PLAID_CLIENT_ID,
   process.env.PLAID_SECRET,
   process.env.PLAID_PUBLIC_KEY,
-  process.env.PLAID_ENV);
+  'https://sandbox.plaid.com');
 
 module.exports = plaidClient;

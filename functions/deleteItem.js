ac20758ecdd0a68c68873e183bd3a7cb8bcc1b50
Harvey Multani <harvey.multani@gmail.com>
2017-11-14 13:29:05 -0800

    Move all item deletion logic into endpoint

--- functions/deleteItem.js
+++ functions/deleteItem.js
@@ -1,12 +1,39 @@
 const functions = require('firebase-functions');
+const item = require('./controllers/itemController');
+const user = require('./controllers/userController');
 const plaidClient = require('./apiClients/plaidClient.js');
 
 const deleteItem = functions.https.onRequest((request, response) => {
   response.header('Access-Control-Allow-Origin', '*');
-  const accessToken = request.body.access_token;
-  plaidClient.deleteItem(accessToken)
-    .then((result) => {
-      response.end('Bank Item Deleted', result);
+  const itemId = request.body.itemToDelete;
+  let uniqueUserId;
+  let accessToken;
+
+  item.getUserIdByItemFromDB(itemId)
+    .then((output) => {
+      uniqueUserId = output;
+    })
+    .then(() => {
+      item.getAccessTokenByItem(itemId)
+        .then((token) => {
+          accessToken = token;
+        })
+        .then(() => {
+          plaidClient.deleteItem(accessToken)
+            .then((result) => {
+              item.deleteItemFromItemsCollection(itemId);
+              return result;
+            })
+            .then((result) => {
+              console.log('28', uniqueUserId);
+              console.log('29', itemId);
+              user.deleteItemFromUserCollection(uniqueUserId, itemId);
+              return result;
+            })
+            .then((result) => {
+              response.end('Bank Item Deleted', result);
+            });
+        });
     });
 });
 

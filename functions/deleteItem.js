6701272d1cd7f0b9a867418cd60fb30c33c324b3
Harvey Multani <harvey.multani@gmail.com>
2017-11-19 14:31:10 -0800

    Refactor using promise.all

--- functions/deleteItem.js
+++ functions/deleteItem.js
@@ -4,30 +4,18 @@ const plaidClient = require('./apiClients/plaidClient.js');
 
 function deleteItem(request, response) {
   const itemId = request.body.itemToDelete;
-  let uniqueUserId;
-  let accessToken;
 
-  item.getUserIdByItemFromDB(itemId)
-    .then((output) => {
-      uniqueUserId = output;
+  Promise.all([item.getUserIdByItemFromDB(itemId), item.getAccessTokenByItem(itemId)])
+    .then((values) => {
+      const uniqueUserId = values[0];
+      const accessToken = values[1];
+      return Promise.all([
+        plaidClient.deleteItem(accessToken),
+        item.deleteItemFromItemsCollection(itemId),
+        user.deleteItemFromUserCollection(uniqueUserId, itemId)]);
     })
-    .then(() => {
-      item.getAccessTokenByItem(itemId)
-        .then((token) => {
-          accessToken = token;
-        })
-        .then(() => {
-          plaidClient.deleteItem(accessToken)
-            .then((result) => {
-              item.deleteItemFromItemsCollection(itemId);
-              user.deleteItemFromUserCollection(uniqueUserId, itemId);
-              return result;
-            })
-            .then((result) => {
-              response.end('Bank Item Deleted', result);
-            });
-        });
-    });
+    .then(result => response.end('Bank Item Deleted', result))
+    .catch(error => console.log(error));
 }
 
 module.exports = deleteItem;

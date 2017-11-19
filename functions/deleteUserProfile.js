62d84549c94a09138c117239dd4bd8eec50069d0
Harvey Multani <harvey.multani@gmail.com>
2017-11-19 15:10:42 -0800

    Refactor deleteUserProfile using promise.all

--- functions/deleteUserProfile.js
+++ functions/deleteUserProfile.js
@@ -5,60 +5,49 @@ const axios = require('axios');
 
 function deleteUserProfile(request, response) {
   const idToken = request.body.idToken;
-  let uniqueUserId;
 
   verifyIdToken(idToken).then((result) => {
-    uniqueUserId = result;
-  })
-    .then(() => {
-      deleteBankItems(uniqueUserId);
-    })
-    .then(() => {
-      deleteCalendar(uniqueUserId);
-    })
-    .then(() => {
-      user.deleteUserInAuth(uniqueUserId)
-        .then(response.end('Profile Deleted'));
-    })
+    const uniqueUserId = result;
+    return Promise.all([
+      deleteBankItems(uniqueUserId),
+      deleteCalendar(uniqueUserId),
+      user.deleteUserInAuth(uniqueUserId)]);
+  }).then(response.end('Profile Deleted'))
     .catch(e => console.log(e));
 }
 
-const deleteBankItems = (uniqueUserId) => {
-  user.getUserItems(uniqueUserId)
-    .then((itemsObj) => {
-      const allItems = Object.keys(itemsObj);
-      allItems.forEach((currentItem) => {
-        item.getItemFromDB(currentItem)
-          .then((itemData) => {
-            const config = {
-              url: `${process.env.HOST}deleteItem`,
-              payload: {
-                itemToDelete: currentItem,
-                access_token: itemData.access_token,
-              },
-            };
-            axios.post(config.url, config.payload)
-              .then(plaidRes => console.log('29', plaidRes.data));
-          });
-      });
+const deleteBankItems = uniqueUserId => user.getUserItems(uniqueUserId)
+  .then((itemsObj) => {
+    const allItems = Object.keys(itemsObj);
+    allItems.forEach((currentItem) => {
+      item.getItemFromDB(currentItem)
+        .then((itemData) => {
+          const config = {
+            url: `${process.env.HOST}deleteItem`,
+            payload: {
+              itemToDelete: currentItem,
+              access_token: itemData.access_token,
+            },
+          };
+          axios.post(config.url, config.payload)
+            .then(plaidRes => console.log(plaidRes.data));
+        });
     });
-};
+  });
 
-const deleteCalendar = (uniqueUserId) => {
-  user.getUserFromDB(uniqueUserId)
-    .then((userData) => {
-      const config = {
-        url: `${process.env.HOST}deleteCalendar`,
-        payload: {
-          calendarId: userData.calendarId,
-          OAuthToken: userData.OAuthToken,
-        },
-      };
-      axios.post(config.url, config.payload)
-        .then(response => console.log('62 Gcal Deletion Response', response.data))
-        .then(user.deleteUserFromDB(uniqueUserId));
-    });
-};
+const deleteCalendar = uniqueUserId => user.getUserFromDB(uniqueUserId)
+  .then((userData) => {
+    const config = {
+      url: `${process.env.HOST}deleteCalendar`,
+      payload: {
+        calendarId: userData.calendarId,
+        OAuthToken: userData.OAuthToken,
+      },
+    };
+    axios.post(config.url, config.payload)
+      .then(response => console.log('62 Gcal Deletion Response', response.data))
+      .then(user.deleteUserFromDB(uniqueUserId));
+  });
 
 module.exports = deleteUserProfile;
 

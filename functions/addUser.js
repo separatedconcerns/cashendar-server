8d93c629829ea94ba7fecefc892287bc73e06f58
Harvey Multani <harvey.multani@gmail.com>
2017-11-20 15:12:07 -0800

    Move tokens and Id to function scope and paralleize user initialization and calendar creation

--- functions/addUser.js
+++ functions/addUser.js
@@ -2,60 +2,56 @@ const user = require('./controllers/userController');
 const verifyIdToken = require('./utils/verifyIdToken.js');
 const axios = require('axios');
 
-let idToken;
-let OAuthToken;
-let uniqueUserId;
-
 function addUser(request, response) {
-  idToken = request.body.idToken;
-  OAuthToken = request.body.OAuthToken;
+  let uniqueUserId;
+  const idToken = request.body.idToken;
+  const OAuthToken = request.body.OAuthToken;
 
   verifyIdToken(idToken)
     .then((result) => {
       uniqueUserId = result;
-      user.doesUserExist(uniqueUserId)
+      return user.doesUserExist(uniqueUserId)
         .then((snapshot) => {
           if (snapshot.exists()) {
-            console.log(21);
-            // end response if user already exists
+            console.log('user exists');
             response.send({ items: snapshot.val().items });
           } else {
             addUserToDB(response); // eslint-disable-line
           }
         });
     });
-}
 
-function addUserToDB(response) {
-  console.log('addUserToDB called');
-  user.getUserProfile(uniqueUserId)
-    .then(userProfile => ({
-      email: userProfile.email,
-      name: userProfile.displayName,
-      OAuthToken,
-    }))
-    .then((payload) => {
-      user.initializeUser(uniqueUserId, payload)
-        .then(() => createGoogleCalendar(response)); // eslint-disable-line
-    });
-}
+  function addUserToDB(response) {
+    console.log('addUserToDB called');
+    return user.getUserProfile(uniqueUserId)
+      .then((userProfile) => {
+        const userData = {
+          email: userProfile.email,
+          name: userProfile.displayName,
+          OAuthToken,
+        };
+        return Promise.all([user.initializeUser(uniqueUserId, userData), createGoogleCalendar(response)]);
+      })
+      .catch(error => console.log(error));
+  }
 
-function createGoogleCalendar(response) {
-  const config = {
-    url: `${process.env.HOST}createNewCalendar`,
-    payload: { OAuthToken, uniqueUserId },
-  };
-  axios.post(config.url, config.payload)
-    .then(calendar => ({
-      calId: calendar.data.id,
-      calName: calendar.data.summary,
-    }))
-    .then((configCal) => {
-      const userCalendarDetails = { calendarId: configCal.calId, calendarName: configCal.calName };
-      user.updateUser(uniqueUserId, userCalendarDetails);
-    })
-    .then(response.end())
-    .catch(error => console.log('Error fetching user data:', error));
+  function createGoogleCalendar(response) {
+    const config = {
+      url: `${process.env.HOST}createNewCalendar`,
+      payload: { OAuthToken, uniqueUserId },
+    };
+    return axios.post(config.url, config.payload)
+      .then(calendar => ({
+        calId: calendar.data.id,
+        calName: calendar.data.summary,
+      }))
+      .then((configCal) => {
+        const userCalendarDetails = { calendarId: configCal.calId, calendarName: configCal.calName };
+        user.updateUser(uniqueUserId, userCalendarDetails);
+      })
+      .then(response.end())
+      .catch(error => console.log('Error fetching user data:', error));
+  }
 }
 
 module.exports = addUser;

7c55845a798b1591bd7a7b323ccc2b0f160dcc01
Harvey Multani <harvey.multani@gmail.com>
2017-11-21 08:19:00 -0800

    Convert to map and remove unnecessary ending markers

--- functions/getTransactionsFromDatabase.js
+++ functions/getTransactionsFromDatabase.js
@@ -3,27 +3,23 @@ const item = require('./controllers/itemController');
 
 function getTransactionsFromDatabase(request, response) {
   const uniqueUserId = request.body.uniqueUserId;
-  user.getUserFromDB(uniqueUserId)
+  let allTransactions = {};
+  return user.getUserFromDB(uniqueUserId)
     .then((userData) => {
       const payload = {
         datesToScheduleQueue: userData.datesToScheduleQueue,
         itemIds: Object.keys(userData.items),
       };
-      let allTransactions = {};
-      const lastItemId = payload.itemIds[payload.itemIds.length - 1];
-      const lastDate = payload.datesToScheduleQueue[payload.datesToScheduleQueue.length - 1];
       const promiseArr = payload.itemIds.map(itemId => item.getItemTransactionsFromDB(itemId)
         .then((transactions) => {
           payload.datesToScheduleQueue.forEach((date) => {
             const mergeObj = Object.assign(allTransactions, transactions[date] || {});
             allTransactions = mergeObj;
-            if (itemId === lastItemId && date === lastDate) {
-              response.json(allTransactions);
-            }
           });
         }));
       return Promise.all(promiseArr);
-    });
+    })
+    .then(() => response.json(allTransactions));
 }
 
 module.exports = getTransactionsFromDatabase;

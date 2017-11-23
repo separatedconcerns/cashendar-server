d750a8fa2a7e2d467082ce51dd5f36dce0ee38c0
Landon Roberts <landonmarkroberts@gmail.com>
2017-11-23 00:56:13 -0600

    Refactor getTransactionsFromDatabase to handle case in which no items are present in db after an item is deleted.

--- functions/getTransactionsFromDatabase.js
+++ functions/getTransactionsFromDatabase.js
@@ -6,24 +6,29 @@ function getTransactionsFromDatabase(request, response) {
   user.getUserFromDB(uniqueUserId)
     .then(userData => ({
       datesToScheduleQueue: userData.datesToScheduleQueue,
-      itemIds: Object.keys(userData.items),
+      itemIds: userData.items ? Object.keys(userData.items) : [],
     }))
     .then((payload) => {
       let allTransactions = {};
       const lastItemId = payload.itemIds[payload.itemIds.length - 1];
       const lastDate = payload.datesToScheduleQueue[payload.datesToScheduleQueue.length - 1];
-      payload.itemIds.forEach((itemId) => {
-        item.getItemTransactionsFromDB(itemId)
-          .then((transactions) => {
-            payload.datesToScheduleQueue.forEach((date) => {
-              const mergeObj = Object.assign(allTransactions, transactions[date] || {});
-              allTransactions = mergeObj;
-              if (itemId === lastItemId && date === lastDate) {
-                response.json(allTransactions);
-              }
+      if (payload.itemIds.length) {
+        payload.itemIds.forEach((itemId) => {
+          item.getItemTransactionsFromDB(itemId)
+            .then((transactions) => {
+              transactions = transactions || {};
+              payload.datesToScheduleQueue.forEach((date) => {
+                const mergeObj = Object.assign(allTransactions, transactions[date] || {});
+                allTransactions = mergeObj;
+                if (itemId === lastItemId && date === lastDate) {
+                  response.json(allTransactions);
+                }
+              });
             });
-          });
-      });
+        });
+      } else {
+        response.json(allTransactions);
+      }
     });
 }
 

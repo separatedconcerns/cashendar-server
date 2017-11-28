914b571be17ca72cd339be47c2c5bcdfedcdeb9b
Harvey Multani <harvey.multani@gmail.com>
2017-11-28 14:24:59 -0800

    Reapply item refactoring

--- functions/controllers/itemController.js
+++ functions/controllers/itemController.js
@@ -1,93 +1,57 @@
 const admin = require('../apiClients/firebaseClient');
 
+const db = ref => admin.database().ref(ref);
+
 exports.deleteItemFromItemsCollection = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}`)
-      .remove()
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}`)
+    .remove()
+    .catch(err => console.log(err));
 
 exports.getItemFromDB = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}`)
-      .once('value')
-      .then(snapshot => resolve(snapshot.val()))
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}`)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .catch(err => console.log(err));
 
 exports.getAccessTokenByItem = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/access_token`)
-      .once('value')
-      .then(snapshot => resolve(snapshot.val()))
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/access_token`)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .catch(err => console.log(err));
 
 exports.getItemTransactionsFromDB = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/transactions`)
-      .once('value')
-      .then(snapshot => resolve(snapshot.val()))
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/transactions`)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .catch(err => console.log(err));
 
 exports.addTransactionsByDate = (itemId, date, transactions) =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`/items/${itemId}/transactions/${date}`)
-      .update(transactions)
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`/items/${itemId}/transactions/${date}`)
+    .update(transactions)
+    .catch(err => console.log(err));
 
 exports.getUserIdByItemFromDB = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/uniqueUserId`)
-      .once('value')
-      .then((snapshot) => {
-        resolve(snapshot.val());
-      })
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/uniqueUserId`)
+    .once('value')
+    .then(snapshot => snapshot.val())
+    .catch(err => console.log(err));
 
 exports.addDataToItem = (itemId, dataToAdd) =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}`)
-      .set(dataToAdd)
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}`)
+    .set(dataToAdd)
+    .catch(err => console.log(err));
 
 exports.removeTransactions = (itemId, dateAndId1, dateAndId2) =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/transactions/${dateAndId1}/${dateAndId2}`)
-      .remove()
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/transactions/${dateAndId1}/${dateAndId2}`)
+    .remove()
+    .catch(err => console.log(err));
 
 exports.addAccountsToItem = (itemId, dataToAdd) =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/accounts/${dataToAdd.account_id}`)
-      .update(dataToAdd)
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/accounts/${dataToAdd.account_id}`)
+    .update(dataToAdd)
+    .catch(err => console.log(err));
 
 exports.removeAllTransactionsInAnItem = itemId =>
-  new Promise((resolve, reject) => {
-    admin.database()
-      .ref(`items/${itemId}/transactions`)
-      .remove()
-      .then(resolve())
-      .catch(err => reject(err));
-  });
+  db(`items/${itemId}/transactions`)
+    .remove()
+    .catch(err => console.log(err));

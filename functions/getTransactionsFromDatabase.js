b9d2e6621f9640afeca93bf812fa94af5ca5d0b5
Harvey Multani <harvey.multani@gmail.com>
2017-11-29 09:48:07 -0800

    Move from forEach to map and remove unnecessary then

--- functions/getTransactionsFromDatabase.js
+++ functions/getTransactionsFromDatabase.js
@@ -4,31 +4,28 @@ const item = require('./controllers/itemController');
 function getTransactionsFromDatabase(request, response) {
   const uniqueUserId = request.body.uniqueUserId;
   user.getUserFromDB(uniqueUserId)
-    .then(userData => ({
-      datesToScheduleQueue: userData.datesToScheduleQueue,
-      itemIds: userData.items ? Object.keys(userData.items) : [],
-    }))
-    .then((payload) => {
+    .then((userData) => {
+      const payload = {
+        datesToScheduleQueue: userData.datesToScheduleQueue,
+        itemIds: userData.items ? Object.keys(userData.items) : [],
+      };
       let allTransactions = {};
       const lastItemId = payload.itemIds[payload.itemIds.length - 1];
       const lastDate = payload.datesToScheduleQueue[payload.datesToScheduleQueue.length - 1];
-      if (payload.itemIds.length) {
-        payload.itemIds.forEach((itemId) => {
-          item.getItemTransactionsFromDB(itemId)
-            .then((transactions) => {
-              transactions = transactions || {};
-              payload.datesToScheduleQueue.forEach((date) => {
-                const mergeObj = Object.assign(allTransactions, transactions[date] || {});
-                allTransactions = mergeObj;
-                if (itemId === lastItemId && date === lastDate) {
-                  response.json(allTransactions);
-                }
-              });
+
+      if (payload.itemIds.length === 0) { response.json(allTransactions); }
+
+      const itemsProm = payload.itemIds.map(itemId =>
+        item.getItemTransactionsFromDB(itemId)
+          .then((transactions) => {
+            transactions = transactions || {};
+            payload.datesToScheduleQueue.forEach((date) => {
+              const mergeObj = Object.assign(allTransactions, transactions[date] || {});
+              allTransactions = mergeObj;
+              if (itemId === lastItemId && date === lastDate) { response.json(allTransactions); }
             });
-        });
-      } else {
-        response.json(allTransactions);
-      }
+          }));
+      return Promise.all(itemsProm);
     });
 }
 

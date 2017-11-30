e475781571418823d8f1e3b2b305a63e203baa61
Landon Roberts <landonmarkroberts@gmail.com>
2017-11-29 19:37:05 -0600

    Refactor packageEventsToSchedule to render transactions in an event by account name

--- functions/utils/packageEventsToSchedule.js
+++ functions/utils/packageEventsToSchedule.js
@@ -1,18 +1,20 @@
 
-const packageEvents = (auth, calendarId, transactionsByDate) => {
-  const dailySpending = transactionsByDate.data;
+const packageEvents = (auth, calendarId, transactionsByDateAndAccount) => {
+  const dailySpending = transactionsByDateAndAccount.data;
   const calEvents = [];
   // eslint-disable-next-line
   for (const date in dailySpending) {
     const sum = Math.round(dailySpending[date].sum);
-    const list = dailySpending[date].list.join('\n');
+    const acctNames = Object.keys(dailySpending[date]);
+    const transactionsByAcctName = acctNames.filter(name => name !== 'sum')
+      .map(name => `${name}:\n${dailySpending[date][name].join('\n')}`);
     const spentOrEarned = sum >= 0 ? 'Spent' : 'Earned';
     const color = spentOrEarned === 'Spent' ? '4' : '2';
     const emoji = spentOrEarned === 'Spent' ? `💸` : `👏`;
 
     const event = {
       summary: `${emoji} ${spentOrEarned} $${Math.abs(sum)} `,
-      description: `Transactions:\n${list}`,
+      description: `${transactionsByAcctName.join('\n')}`,
       colorId: color,
       start: {
         date,

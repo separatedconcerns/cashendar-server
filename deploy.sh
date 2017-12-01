ebd97050d0ae4e3a23c4ed84b7d0d04011066e2c
Harvey Multani <harvey.multani@gmail.com>
2017-11-30 16:10:41 -0800

    fix directory for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" -a $"$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "$TRAVIS_BRANCH"
+  cd functions
+  echo $(pwd)
   ./node_modules/.bin/firebase deploy -P dev --token $FIREBASE_DEPLOY_TOKEN
   exit 0
 else
+  cd functions
+  echo $(pwd)
   echo "Will only deploy if on master and not a PR"
   exit 0
 fi

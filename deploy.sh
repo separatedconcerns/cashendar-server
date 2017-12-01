36a61096974c7e414316c9bb8bb4b4938d2ab77d
Harvey Multani <harvey.multani@gmail.com>
2017-12-01 14:18:07 -0800

    fix deploy typo

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ then
   chmod +x envkey-fetch-linux
   ./envkey-fetch-linux $ENVKEY_PRODUCTION > src/creds.json
   npm run build
-  ./node_modules/.bin/firebase deploy -P dev --token $FIREBASE_PRODUCTION_DEPLOY_TOKEN 
+  ./node_modules/.bin/firebase deploy -P production --token $FIREBASE_PRODUCTION_DEPLOY_TOKEN 
 else
   echo "Will only deploy if not a pull request and branch is either master or staging"
   exit 0

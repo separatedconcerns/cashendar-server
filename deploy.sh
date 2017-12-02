afe67edd5af6afabcf7c94a23719725af3e7865d
Harvey Multani <harvey.multani@gmail.com>
2017-12-02 11:31:56 -0800

    Deploy on branch condition

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" -a $"$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_BRANCH" == "master" -a $"$TRAVIS_PULL_REQUEST" == "false" ]; 
+then
   echo "$TRAVIS_BRANCH"
-  cd functions
-  echo $(pwd)
-  ./node_modules/.bin/firebase deploy --only functions -P dev --token $FIREBASE_DEPLOY_TOKEN
+  ./node_modules/.bin/firebase deploy -P staging --token $FIREBASE_STAGING_DEPLOY_TOKEN
   exit 0
+elif [ "$TRAVIS_BRANCH" == "production" -a $"$TRAVIS_PULL_REQUEST" == "false" ];
+then
+  echo "$TRAVIS_BRANCH"
+  ./node_modules/.bin/firebase deploy -P production --token $FIREBASE_PRODUCTION_DEPLOY_TOKEN 
 else
-  cd functions
-  echo $(pwd)
-  echo "Will only deploy if on master and not a PR"
+  echo "Will only deploy if not a pull request and branch is either master or staging"
   exit 0
 fi

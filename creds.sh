4682b8ae62ff725f5496318eb7f5d192e3df059f
Harvey Multani <harvey.multani@gmail.com>
2017-12-02 11:31:37 -0800

    generate creds

---
+++ creds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" -a $"$TRAVIS_PULL_REQUEST" == "false" ]; 
+then
+  echo "$TRAVIS_BRANCH"
+  chmod +x envkey-fetch-linux
+  ./envkey-fetch-linux $ENVKEY_STAGING > functions/creds.json
+  exit 0
+elif [ "$TRAVIS_BRANCH" == "production" -a $"$TRAVIS_PULL_REQUEST" == "false" ];
+then
+  echo "$TRAVIS_BRANCH"
+  chmod +x envkey-fetch-linux
+  ./envkey-fetch-linux $ENVKEY_PRODUCTION > functions/creds.json
+else
+  echo "couldn't find creds"
+  exit 0
+fi

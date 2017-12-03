f64b48a85bbd3db6a2cac11d667e6d6b19551272
Harvey Multani <harvey.multani@gmail.com>
2017-12-03 12:50:22 -0800

    move scripts and env generator to functions folder

--- functions/creds.sh
+++ functions/creds.sh
@@ -3,13 +3,13 @@ if [ "$TRAVIS_BRANCH" == "master" -a $"$TRAVIS_PULL_REQUEST" == "false" ];
 then
   echo "$TRAVIS_BRANCH"
   chmod +x envkey-fetch-linux
-  ./envkey-fetch-linux $ENVKEY_STAGING > functions/creds.json
+  ./envkey-fetch-linux $ENVKEY_STAGING > creds.json
   exit 0
 elif [ "$TRAVIS_BRANCH" == "production" -a $"$TRAVIS_PULL_REQUEST" == "false" ];
 then
   echo "$TRAVIS_BRANCH"
   chmod +x envkey-fetch-linux
-  ./envkey-fetch-linux $ENVKEY_PRODUCTION > functions/creds.json
+  ./envkey-fetch-linux $ENVKEY_PRODUCTION > creds.json
 else
   echo "couldn't find creds"
   exit 0

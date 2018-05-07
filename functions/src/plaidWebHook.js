2f7fccc2ba56f4bf50f1ea5183178eec2d96502d
Harvey Multani <harvey.multani@gmail.com>
2018-05-07 15:52:59 -0700

    Split out internal functions for easier comprehension

--- functions/src/plaidWebHook.js
+++ functions/src/plaidWebHook.js
@@ -2,46 +2,54 @@ import { post } from 'axios';
 import { getItemFromDB } from './controllers/itemController';
 import { HOST } from './creds.json';
 
-async function plaidWebHook(request, response) {
+function processHistorical() {
+
+}
+
+async function processTransactionsRemoved(itemId, removedTransactions) {
+  const config = {
+    url: `${HOST}removeTransactionsFromDb`,
+    payload: {
+      itemId,
+      removedTransactions,
+    },
+  };
+
+  const uniqueUserId = await post(config.url, config.payload);
+  await post(`${HOST}addCalendarEvents`, { uniqueUserId: uniqueUserId.data });
+}
+
+async function processInitial(itemId, newTransactions) {
+  const itemData = await getItemFromDB(itemId);
+  if (itemData) {
+    const config = {
+      url: `${HOST}getTransactionsFromPlaid`,
+      payload: {
+        accessToken: itemData.accessToken,
+        uniqueUserId: itemData.uniqueUserId,
+        newTransactions,
+      },
+    };
+    await post(config.url, config.payload);
+    await post(`${HOST}addCalendarEvents`, config.payload);
+    // .catch(e => console.log('plaidWebHook Error!:', e));
+  }
+  return ' ';
+}
+
+export default async function plaidWebHook(request, response) {
   const itemId = request.body.item_id;
   const webHookCode = request.body.webhook_code;
   const newTransactions = request.body.new_transactions || null;
-  console.log('WEBHOOK HIT:', itemId, webHookCode, newTransactions);
+  const removedTransactions = request.body.removed_transactions || null;
+  console.log('WEBHOOK HIT:', itemId, webHookCode, newTransactions, removedTransactions);
 
   if (webHookCode === 'HISTORICAL_UPDATE') {
-    response.end();
+    processHistorical();
   } else if (webHookCode === 'TRANSACTIONS_REMOVED') {
-    const config = {
-      url: `${HOST}removeTransactionsFromDb`,
-      payload: {
-        itemId,
-        removedTransactions: request.body.removed_transactions,
-      },
-    };
-
-    const uniqueUserId = await post(config.url, config.payload);
-    await post(`${HOST}addCalendarEvents`, { uniqueUserId: uniqueUserId.data });
-    response.end();
-    // .catch(e => console.log('TRANSACTIONS_REMOVED ERROR!:', e));
+    processTransactionsRemoved(itemId, removedTransactions);
   } else {
-    const itemData = await getItemFromDB(itemId);
-    if (itemData) {
-      const config = {
-        url: `${HOST}getTransactionsFromPlaid`,
-        payload: {
-          access_token: itemData.access_token,
-          uniqueUserId: itemData.uniqueUserId,
-          newTransactions,
-        },
-      };
-      await post(config.url, config.payload);
-      await post(`${HOST}addCalendarEvents`, config.payload);
-      response.end();
-      // .catch(e => console.log('plaidWebHook Error!:', e));
-    } else {
-      response.end();
-    }
+    processInitial(itemId, newTransactions);
   }
+  response.end();
 }
-
-export default plaidWebHook;

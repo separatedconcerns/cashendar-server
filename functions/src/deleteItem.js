f1163e634683be0f516264fe051f4e1810a619da
Harvey Multani <harvey.multani@gmail.com>
2018-05-07 22:00:46 -0700

    Add try catch blocks

--- functions/src/deleteItem.js
+++ functions/src/deleteItem.js
@@ -1,19 +1,25 @@
 import { post } from 'axios';
 import { getUserIdByItemFromDB, getAccessTokenByItem, getItemTransactionsFromDB, deleteItemFromItemsCollection } from './controllers/itemController';
-import { updateDatesToScheduleQueue, deleteItemFromUserCollection } from './controllers/userController';
+import { doesUserExist, updateDatesToScheduleQueue, deleteItemFromUserCollection } from './controllers/userController';
 import { deleteItem as _deleteItem } from './apiClients/plaidClient';
 import { HOST } from './creds.json';
 
 async function deleteItem(request, response) {
   const itemId = request.body.itemToDelete;
-  const [uniqueUserId, accessToken] = await Promise.all([getUserIdByItemFromDB(itemId), getAccessTokenByItem(itemId)]);
-  await _deleteItem(accessToken);
-  const transactions = await getItemTransactionsFromDB(itemId);
-  const transactionDates = Object.keys(transactions);
-  await updateDatesToScheduleQueue(uniqueUserId, transactionDates);
-  await Promise.all([deleteItemFromItemsCollection(itemId), deleteItemFromUserCollection(uniqueUserId, itemId)]);
-  const result = await post(`${HOST}addCalendarEvents`, { uniqueUserId });
-  response.end('Bank Item Deleted', result);
+  try {
+    const [uniqueUserId, accessToken] = await Promise.all([getUserIdByItemFromDB(itemId), getAccessTokenByItem(itemId)]);
+    await _deleteItem(accessToken);
+    const transactions = await getItemTransactionsFromDB(itemId);
+    const transactionDates = Object.keys(transactions);
+    const userExists = await doesUserExist(uniqueUserId);
+    if (!userExists) { response.end('Bank Item Deleted'); }
+    await updateDatesToScheduleQueue(uniqueUserId, transactionDates);
+    await Promise.all([deleteItemFromItemsCollection(itemId), deleteItemFromUserCollection(uniqueUserId, itemId)]);
+    const result = await post(`${HOST}addCalendarEvents`, { uniqueUserId });
+    response.end('Bank Item Deleted', result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default deleteItem;

0fc9730f3b91db2949e89abe689306914296b960
Harvey Multani <harvey.multani@gmail.com>
2018-07-13 17:40:51 +0200

    move to async/await

--- functions/utils/deleteDuplicateEventsFlow.js
+++ functions/utils/deleteDuplicateEventsFlow.js
@@ -2,31 +2,42 @@ import { post } from 'axios';
 import { getDatesToScheduleQueueFromDB, updateScheduledEvents, clearDatesToScheduleAndEventsToDeleteQueues } from '../src/controllers/userController';
 import { HOST } from '../src/creds.json';
 
-const deleteDuplicateEventsFlow = (uniqueUserId, newEvents) =>
-  getDatesToScheduleQueueFromDB(uniqueUserId)
-    .then((datesToScheduleQueue) => {
-      datesToScheduleQueue.forEach((date) => {
-        newEvents[date] = newEvents[date] || null;
-      });
-      const config = {
-        url: `${HOST}createEventsToDeleteQueueInDb`,
-        payload: {
-          newEventDates: Object.keys(newEvents),
-          uniqueUserId,
-        },
-      };
-      return post(config.url, config.payload);
-    })
-    .then((calId_eventsToDeleteQueue_OAuthToken) => {
-      const config2 = {
-        url: `${HOST}deleteDuplicateEventsInCalendar`,
-        payload: calId_eventsToDeleteQueue_OAuthToken.data,
-      };
-      return post(config2.url, config2.payload);
-    })
-    .then(() => Promise.all(
-      [updateScheduledEvents(uniqueUserId, newEvents),
-        clearDatesToScheduleAndEventsToDeleteQueues(uniqueUserId)]))
-    .catch(e => console.log('Promise.all error!:', e));
+async function mutateNewEvents(uniqueUserId, newEvents) {
+  const eventsToMutate = newEvents;
+  let datesToScheduleQueue;
+  try {
+    datesToScheduleQueue = await getDatesToScheduleQueueFromDB(uniqueUserId);
+  } catch (error) {0
+    console.log(error);
+  }
+  datesToScheduleQueue.forEach((date) => {
+    eventsToMutate[date] = newEvents[date] || null;
+  });
+  return eventsToMutate;
+}
+
+async function getToken(uniqueUserId, newEvents) {
+  const config = {
+    url: `${HOST}createEventsToDeleteQueueInDb`,
+    payload: {
+      newEventDates: Object.keys(newEvents),
+      uniqueUserId,
+    },
+  };
+  return post(config.url, config.payload);
+}
+
+const deleteDuplicateEventsFlow = async (uniqueUserId, newEvents) => {
+  const mutatedEvents = await mutateNewEvents(uniqueUserId, newEvents);
+  const calIdEventsToDeleteQueueOAuthToken = await getToken(uniqueUserId, mutatedEvents);
+  const config2 = {
+    url: `${HOST}deleteDuplicateEventsInCalendar`,
+    payload: calIdEventsToDeleteQueueOAuthToken.data,
+  };
+  await post(config2.url, config2.payload);
+  await Promise.all([
+    updateScheduledEvents(uniqueUserId, newEvents),
+    clearDatesToScheduleAndEventsToDeleteQueues(uniqueUserId)]);
+};
 
 module.exports = deleteDuplicateEventsFlow;
